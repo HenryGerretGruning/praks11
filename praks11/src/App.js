fde077d4e0c2a569b45f6b431d440c7d97d9a177
Henry-Gerret Grüning <artimax32@gmail.com>
2021-01-25 11:37:58 +0200

    Fixed json funtion

--- praks11/src/App.js
+++ praks11/src/App.js
@@ -1,25 +1,80 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
 
-function App() {
+
+const App = () => {
+  
+  const [recipes , setRecipes] = useState(0);
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  function getRecipes(){
+    fetch("data/data.json", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+    .then((response) => {console.log(response)
+
+        return response.json();
+    })
+      // response.text() returns a new promise that resolves with the full response text
+      // when it loads
+    .then((data) => {
+
+      setRecipes(data);
+    })
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <main>
+      <h1>Retseptiraamat</h1>
+      <RecipeList recipes={recipes}></RecipeList>
+      
+    </main>
+  )
+}
+
+
+
+
+
+const Recipe = (props) => {
+  
+  return(
+    
+    <div>
+      <h1>smile</h1>
+      
+      <h2>{props.recipe.name} </h2>
+      <p> {props.recipe.duration} </p>
+      
+      <p>{props.recipe.preparation}</p>
     </div>
-  );
+    
+  )
+}
+
+const RecipeList = (props) => {
+  console.log(props.recipes)
+  const recipes = props.recipes
+
+  return(
+    
+      <div>
+        
+       <div>
+
+        <h2> </h2>
+        <p>  </p>
+
+       </div>
+
+      </div>
+    
+  )
 }
 
 export default App;
